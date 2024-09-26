b7c02bed53a6aad3fc886826721aa12864d5f315
lprimak <lenny@flowlogix.com>
2024-09-25 23:13:57 -0500

    enh(archetype): adding apache header to each generated java file automatically

--- archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,4 +1,18 @@
-def file = new File(request.getOutputDirectory(), request.getArtifactId() + "/mvnw");
-file.setExecutable(true, false);
-file = new File(request.getOutputDirectory(), request.getArtifactId() + "/mvnw.cmd");
-file.setExecutable(true, false);
+// set the executable flag on the mvnw and mvnw.cmd files
+def mavenExecutable = new File(request.getOutputDirectory(), request.getArtifactId() + "/mvnw")
+mavenExecutable.setExecutable(true, false)
+mavenExecutable = new File(request.getOutputDirectory(), request.getArtifactId() + "/mvnw.cmd")
+mavenExecutable.setExecutable(true, false)
+
+// prepend apache-header.txt to all Java files
+def headerText = new File(request.getOutputDirectory(), request.getArtifactId() + "/src/checkstyle/apache-header.txt").text
+def javaDirectories = []
+javaDirectories << new File(request.getOutputDirectory(), request.getArtifactId() + "/src/main/java")
+javaDirectories << new File(request.getOutputDirectory(), request.getArtifactId() + "/src/test/java")
+javaDirectories.each { it.eachFileRecurse {
+    javaFile ->
+        if (javaFile.isFile() && javaFile.getName().endsWith(".java")) {
+            javaFile.write(headerText + javaFile.text)
+        }
+    }
+}
