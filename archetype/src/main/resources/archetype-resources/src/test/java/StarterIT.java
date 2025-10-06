eb9b8d3fb03175bae555ee33b2810db9d6ebf1cc
lprimak <lenny@flowlogix.com>
2025-10-06 17:10:15 -0500

    enh(test): replace `@ExtendWith` annotations with `@PayaraServerLifecycle` for streamlined server lifecycle management

--- archetype/src/main/resources/archetype-resources/src/test/java/StarterIT.java
+++ archetype/src/main/resources/archetype-resources/src/test/java/StarterIT.java
@@ -1,17 +1,14 @@
 package $package;
 
-import com.flowlogix.testcontainers.PayaraServerLifecycleExtension;
+import com.flowlogix.test.PayaraServerLifecycle;
 import com.flowlogix.util.ShrinkWrapManipulator;
 import org.eu.ingwar.tools.arquillian.extension.suite.annotations.ArquillianSuiteDeployment;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit5.ArquillianExtension;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(PayaraServerLifecycleExtension.class)
-@ExtendWith(ArquillianExtension.class)
+@PayaraServerLifecycle
 @ArquillianSuiteDeployment
 class StarterIT {
     @Test
